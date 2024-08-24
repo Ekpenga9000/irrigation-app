16d0678d82cf5e7e8ec246a99bc5feb6dc257564
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-08-23 22:00:47 -0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>{" "}
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import { PiPlantBold } from "react-icons/pi";
+import { RxDashboard } from "react-icons/rx";
+import { LuLogIn } from "react-icons/lu";
+import { TbLogout2 } from "react-icons/tb";
+
+const Navbar = () => {
+  return (
+    <header className="p-4">
+      <nav className="flex items-center justify-between">
+        <Link to="/" className="flex items-center gap-2">
+          <span className="h-[2.5rem] w-[2.5rem] flex  rounded-full bg-teal-500 rotate-3 relative">
+            <PiPlantBold className="text-2xl absolute top-[-0.5rem] left-[-0.5rem]" />
+          </span>
+          <span className="font-semibold text-2xl">
+            Aqua<span className="text-orange-400">Sense</span>
+          </span>
+        </Link>
+
+        <ul className="flex items-center gap-6">
+          <li>
+            <Link to="/dashboard" className="font-semibold text-lg flex items-center gap-1"><span><RxDashboard /></span>Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/login" className="font-semibold text-lg flex items-center gap-1"><LuLogIn />Login</Link>
+          </li>
+          <li>
+            <Link to="/" className="font-semibold text-lg flex items-center gap-1"><TbLogout2 />Logout</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
